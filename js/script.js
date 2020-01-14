b346e268f1a0289a63cb06a5bbddb51dd6d995f2
luca <luca.carnaroli98@gmail.com>
2020-01-14 21:17:16 +0100

    random

--- js/script.js
+++ js/script.js
@@ -1,22 +1,25 @@
 // Il computer deve generare 16 numeri casuali da 1 a 100.
+// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+var numRandom = random(1, 100);
+var arrayRandom = [];
 for (var i = 0; i < 16; i++) {
-  var numRandom = Math.floor(Math.random()*(100 + 1 - 1))+1;
-  console.log(numRandom[i]);
+  arrayRandom.push(random(1, 100));
 }
-// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100,
-for (var i = 0; i <10; i++) {
-  var numUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
-  if (numUtente > 100) {
-    alert('inserisci un numero da 1 a 100!!!');
-  } else if (numUtente < 1) {
-    alert('inserisci un numero da 1 a 100!!!');
-  } else {
-    console.log(numUtente);
+console.log(arrayRandom);
+
+
+function random(min, max){
+  for (var i = min; i < max; i++) {
+    randomNum = Math.floor(Math.random() * (max - min + 1) ) + min;
   }
-  if (numUtente % numRandom == 1) {
-    console.log('fine');
-  } else {
-    console.log();
-  }
+  return randomNum
 }
-// se il numero è presente nella lista dei numeri generati, la partita termina,
+function general(numMin, numMax) {
+  for (var i = numMin; i < numMax; i++) {
+    generalNum = Math.floor(Math.random() * numMax) + numMin;
+  }
+  return generalNum
+}
